d56ad45f15f618ad1f500a0f39e969da10ba5c95
DannyT <84480623+DanthatBoi@users.noreply.github.com>
2024-03-08 15:00:19 -0500

    Update sql.sql
    Adding course into database

--- RateMyCourse/sql.sql
+++ RateMyCourse/sql.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO Course 
+VALUES  ("Software Engineering 2", 415, "Students will collaborate to solve problems and develop software applications with software techniques.", "Mrs, Sultana"),
+        ("Discrete Mathematics", 170, "Deals with objects that can assume with certain values." , "Mrs, Bencharot"), 
+SELECT * FROM Course 
