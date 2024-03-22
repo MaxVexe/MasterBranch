3f1cfccd377c6892e3dcc78b1e4e8abda2d42a61
DannyT <84480623+DanthatBoi@users.noreply.github.com>
2024-03-22 15:51:49 -0400

    Update sql.sql
    Created sample course catalog

--- RateMyCourse/sql.sql
+++ RateMyCourse/sql.sql
@@ -1,4 +1,8 @@
-INSERT INTO Course 
-VALUES  ("Software Engineering 2", 415, "Students will collaborate to solve problems and develop software applications with software techniques.", "Mrs, Sultana"),
+INSERT or IGNORE INTO Course (name, id, courseDesc, Professor)
+VALUES  ("Intro to Literature", 111, "An introduction to reading and analyzing these primary genres of literature: fiction, poetry, and drama.", "Keith Slocum, David Galef, Jeffrey Gonzalez"),
         ("Discrete Mathematics", 170, "Deals with objects that can assume with certain values." , "Mrs, Bencharot"), 
-SELECT * FROM Course 
+        ("Religions of the World", 100, "The major religious traditions, with emphasis on basic beliefs and on the nature and diversity of religious awareness.", "Michael Lees" ),
+        ("Intro to American Studies", 110, "Social, Cultural, Intellectual, Economic, and Political History of the United States.", "Joseph Digianni" ),
+        ("Macroeconomics", 102, "Students will be introduced to the mechanisms needed for the achievement of price stability, full employment level of national income and long-term growth by analyzing the macroeconomic data and the implications of fiscal and monetary policies.", "Lee Sunhqung");
+
+Select * from Course;
