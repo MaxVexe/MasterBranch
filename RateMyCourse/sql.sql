ee6638477bb942efaf57a6b1ed0ddfca23baaad8
DannyT <84480623+DanthatBoi@users.noreply.github.com>
2024-03-22 15:54:37 -0400

    Update sql.sql

--- RateMyCourse/sql.sql
+++ RateMyCourse/sql.sql
@@ -1,1 +1,8 @@
+INSERT or IGNORE INTO Course (name, id, courseDesc, Professor)
+VALUES  ("Intro to Literature", 111, "An introduction to reading and analyzing these primary genres of literature: fiction, poetry, and drama.", "Keith Slocum, David Galef, Jeffrey Gonzalez"),
+        ("Discrete Mathematics", 170, "Deals with objects that can assume with certain values." , "Mrs, Bencharot"), 
+        ("Religions of the World", 100, "The major religious traditions, with emphasis on basic beliefs and on the nature and diversity of religious awareness.", "Michael Lees" ),
+        ("Intro to American Studies", 110, "Social, Cultural, Intellectual, Economic, and Political History of the United States.", "Joseph Digianni" ),
+        ("Macroeconomics", 102, "Students will be introduced to the mechanisms needed for the achievement of price stability, full employment level of national income and long-term growth by analyzing the macroeconomic data and the implications of fiscal and monetary policies.", "Lee Sunhqung");
 
+Select * from Course;
